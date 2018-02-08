2d0d63a611f0b9e5b1638e4109e8b6bc7795a380
sampley <sampley@ualberta.ca>
2018-02-08 13:13:39 -0700

    Added mixer for inputs and softmax for duration output

--- model.py
+++ model.py
@@ -4,8 +4,9 @@ class Net:
     def __init__(self, session, params):
         self.session = session
 
-        PITCHES = 12 # pitches in an octave
-        OCTAVES = 10 # octaves in a midi file
+        PITCHES = 12  # pitches in an octave
+        OCTAVES = 10  # octaves in a midi file
+        DURATIONS = 8 # number of categories of duration
 
         initializer = tf.contrib.layers.xavier_initializer() # initializer for fc layers
 
@@ -14,7 +15,7 @@ class Net:
         LEARNING_RATE         = params['LEARNING_RATE']
 
         self.notes = tf.placeholder(tf.float32, (None, None, PITCHES, OCTAVES), 'notes')  # (time, batch, pitch, octave)
-        self.durations = tf.placeholder(tf.float32, (None, None, 8), 'durations')  # (time, batch, duration_category)
+        self.durations = tf.placeholder(tf.float32, (None, None, DURATIONS), 'durations')  # (time, batch, duration_category)
 
         # cut off last note as input, since we have no corresponding output
         # in the case of a single input, don't cut, because we're doing prediction (training makes no sense without an output)
@@ -37,10 +38,39 @@ class Net:
         # Set variable for dropout of each layer
         self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
+        # Create variables for a couple of useful values
         with tf.name_scope("time_steps"):
             self.times_in   = tf.shape(self.notes_in)[0]
         with tf.name_scope("batch_size"):
             self.batch_size = tf.shape(self.notes_in)[1]
+
+        with tf.name_scope("input_mixer"):
+            # take (time, batch, pitch, octave) to (time, batch, duration)
+            # followed by a non-linear unit
+            # then weighted sum with durations
+            # followed by a non-linear unit
+            with tf.name_scope("durations_with_notes"):
+                W = tf.Variable(initializer((PITCHES, OCTAVES, DURATIONS)), name='W')
+                b = tf.Variable(initializer((DURATIONS,)), name='b')
+                n2d = tf.tensordot(self.notes_in, W, [[2,3],[0,1]]) + b
+                n2d = tf.nn.relu(n2d)
+                Wn2d = tf.Variable(initializer((DURATIONS,)), name='Wn2d')
+                Wd = tf.Variable(initializer((DURATIONS,)), name='Wd')
+                durations_with_notes = Wn2d * n2d + Wd * self.durations_in
+                durations_with_notes = tf.nn.relu(durations_with_notes)
+            # take (time, batch, duration) to (time, batch, pitch, octave)
+            # followed by a non-linear unit
+            # then weighted sum with durations
+            # followed by a non-linear unit
+            with tf.name_scope("notes_with_durations"):
+                W = tf.Variable(initializer((DURATIONS, PITCHES, OCTAVES)), name='W')
+                b = tf.Variable(initializer((PITCHES, OCTAVES)), name='b')
+                d2n = tf.tensordot(self.durations_in, W, [[2],[0]]) + b
+                d2n = tf.nn.relu(d2n)
+                Wd2n = tf.Variable(initializer((PITCHES, OCTAVES)), name='Wd2n')
+                Wn = tf.Variable(initializer((PITCHES, OCTAVES)), name='Wn')
+                notes_with_durations = Wd2n * d2n + Wn * self.notes_in
+                notes_with_durations = tf.nn.relu(notes_with_durations)
 
         # create convolutional rnn with notes as input (time, batch, pitch, octave)
         # output (time, batch, pitch, octave)
@@ -49,20 +79,22 @@ class Net:
             cells[0] = Net.conv_cell(2, [PITCHES, OCTAVES, 1], RNN_NOTES_CHANNELS[0], [5,5], self.keep_prob)
             for i in range(1, len(RNN_NOTES_CHANNELS)):
                 cells[i] = Net.conv_cell(2, [PITCHES, OCTAVES, RNN_NOTES_CHANNELS[i-1]], RNN_NOTES_CHANNELS[i], [5,5], self.keep_prob)
-            cells[-1] = Net.conv_cell(2, [PITCHES, OCTAVES, RNN_NOTES_CHANNELS[-1]], 1, [5,5], self.keep_prob)
+            cells[-1] = Net.conv_cell(2, [PITCHES, OCTAVES, RNN_NOTES_CHANNELS[-1]], 1, [3,3], self.keep_prob)
             cell = tf.nn.rnn_cell.MultiRNNCell(cells, state_is_tuple=True)
             with tf.name_scope("state"):
                 states = Net.get_state_variables(self.batch_size, cell)
-            rnn_notes_output, rnn_notes_state = tf.nn.dynamic_rnn(cell, tf.expand_dims(self.notes_in, -1), initial_state=states, time_major=True, scope=scope)
+            rnn_notes_output, rnn_notes_state = tf.nn.dynamic_rnn(cell, tf.expand_dims(notes_with_durations, -1), initial_state=states, time_major=True, scope=scope)
             rnn_notes_output = tf.squeeze(rnn_notes_output, -1)
 
         # create rnn with durations as input (time, batch, duration)
         # output (time, batch, duration)
+        # end with softmax for duration
         with tf.name_scope("rnn_duration") as scope:
-            cell = Net.cell(RNN_DURATION_CHANNELS + [8], self.keep_prob)
+            cell = Net.cell(RNN_DURATION_CHANNELS + [DURATIONS], self.keep_prob)
             with tf.name_scope("state"):
                 states = Net.get_state_variables(self.batch_size, cell)
-            rnn_duration_output, rnn_duration_state = tf.nn.dynamic_rnn(cell, self.durations_in, initial_state=states, time_major=True, scope=scope)
+            rnn_duration_output, rnn_duration_state = tf.nn.dynamic_rnn(cell, durations_with_notes, initial_state=states, time_major=True, scope=scope)
+            rnn_duration_output = tf.nn.softmax(rnn_duration_output, axis=-1)
 
         # create tuple of rnn states
         self.rnn_state = rnn_notes_state + rnn_duration_state
